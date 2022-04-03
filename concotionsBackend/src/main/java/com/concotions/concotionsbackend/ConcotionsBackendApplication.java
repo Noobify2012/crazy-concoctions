accf17c8a046a279b532910e7e45b7966584fa4e
Daniel Blum <blum.da@northeastern.edu>
2022-04-03 13:37:29 -0400

    adding initial backend project

---
+++ concotionsBackend/src/main/java/com/concotions/concotionsbackend/ConcotionsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.concotions.concotionsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcotionsBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConcotionsBackendApplication.class, args);
+  }
+
+}
---
+++ concotionsBackend/src/test/java/com/concotions/concotionsbackend/ConcotionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concotions.concotionsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcotionsBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
