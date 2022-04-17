a1bfb62b38c65cf4468303d38472172d48cecd70
Daniel Blum <blum.da@northeastern.edu>
2022-04-17 14:18:21 -0400

    just more cleanup cause is stupid

--- concotionsBackend/src/main/java/com/concotions/concotionsbackend/data/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.concotions.concotionsbackend.data;
-
-import com.concotions.concotionsbackend.model.User;
-
-import java.util.List;
-
-public interface UserRepo {
-  List<User> getAllUsers();
-}
