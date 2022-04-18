df8892b14fe96e0afef6ac14c5e755b19e78d116
Daniel Blum <blum.da@northeastern.edu>
2022-04-18 14:18:04 -0400

    updating format on how to load initial sql scripts into docker

--- docker/01_init.sql
+++ docker/01_init.sql
@@ -113,12 +113,3 @@ CREATE TABLE pairing (
 );
 
 
-INSERT INTO user (email, username, password, firstName, lastName, bio) 
-VALUES
-('blum.da@northeastern.edu', 'schooper', 'password', 'Daniel', 'Blum', 
-  "This is a bio I've written!"),
-('greene.matthew@northeastern.edu', 'bro', 'brobro', 'Matt', 'Greene',
-  "This is a stupid bio, I didn't write it.");
-
-
-
---
+++ docker/02_starterData.sql
@@ -0,0 +1,11 @@
+-- starter data for the concoctions database
+
+
+INSERT INTO user (email, username, password, firstName, lastName, bio) 
+VALUES
+('blum.da@northeastern.edu', 'schooper', 'password', 'Daniel', 'Blum', 
+  "This is a bio I've written!"),
+('greene.matthew@northeastern.edu', 'bro', 'brobro', 'Matt', 'Greene',
+  "This is a stupid bio, I didn't write it.");
+
+
